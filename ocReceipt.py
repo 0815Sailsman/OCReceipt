d10b05bb45d39958bbc20504dbd11cf12d945c6e
0815Sailsman <sailsman@tutanota.com>
2024-07-28 14:49:12 +0200

    extract the algo to function

--- ocReceipt.py
+++ ocReceipt.py
@@ -25,45 +25,10 @@ def main():
 
     # check every row by itself as a starting point
     for row_index in range(0, img2.shape[1]):
-        print("starting algo for row " + str(row_index) + "...")
-        row_steps = []
-        actual_row_index = row_index
-        # for column_index in range(0, int((img2.shape[0] / 5) - 1)):
-        for column_index in range(0, img2.shape[0] - 1):
-            block_brightnesses = dict([(0, 255), (-1, 255), (1, 255)])
-            allowed_height_diffs = [0]
-            if actual_row_index - row_index <= 20:
-                allowed_height_diffs.append(-1)
-                allowed_height_diffs.append(-2)
-                allowed_height_diffs.append(-3)
-            if row_index - actual_row_index <= 20:
-                allowed_height_diffs.append(1)
-                allowed_height_diffs.append(2)
-                allowed_height_diffs.append(3)
-            for height_diff in allowed_height_diffs:
-                actual_height = actual_row_index + height_diff
-                if actual_height < 0 or actual_height >= img2.shape[1]:
-                    block_brightnesses[height_diff] = -1
-                    continue
-                pixels = []
-#                for step in range(5):
-#                    pixels.append(img2[column_index*5 + step, actual_height])
-                pixels.append(img2[column_index, actual_height])
-                block_bright = map(lambda pxl: pxl[2]*0.2126+pxl[1]*0.7152+pxl[0]*0.0722, pixels)
-                block_brightnesses[height_diff] = math.ceil(sum(block_bright))
-            brightest = max(block_brightnesses, key=block_brightnesses.get)
-            if brightest == 0 and block_brightnesses[0] == block_brightnesses[1] and len(row_steps) > 100 and sum([0 if x[0] == 0 else 1 for x in row_steps][-100:]) == 0:
-                tendency = 1 if sum([x[0] for x in row_steps]) > 0 else -1
-                row_steps.append((tendency, block_brightnesses[tendency]))
-                actual_row_index += tendency
-            else:
-                row_steps.append((brightest, block_brightnesses[brightest]))
-                actual_row_index += brightest
-        avg_row_brightness = sum(map(lambda row:row[1], row_steps)) / (len(row_steps))
-        if int(avg_row_brightness) >= 254:
-            print("storing row for later!")
-            rows_to_color.append((row_index, [instruction[0] for instruction in row_steps]))
-
+        result = algo(row_index, img2)
+        if result is None:
+            continue
+        rows_to_color.append(result)
 
     for obj in rows_to_color:
         start = obj[0]
@@ -97,6 +62,44 @@ def main():
     # extract rows / items
     # Preprocess rows / items for OCR preprocessen
     # OCR
+
+def algo(row_index, img):
+    print("starting algo for row " + str(row_index) + "...")
+    row_steps = []
+    actual_row_index = row_index
+    # for column_index in range(0, int((img2.shape[0] / 5) - 1)):
+    for column_index in range(0, img.shape[0] - 1):
+        block_brightnesses = dict([(0, 0), (-1, 0), (1, 0), (-2, 0), (2, 0), (-3, 0), (3, 0)])
+        allowed_height_diffs = [0]
+        if actual_row_index - row_index <= 25:
+            allowed_height_diffs.append(-1)
+            allowed_height_diffs.append(-2)
+            allowed_height_diffs.append(-3)
+        if row_index - actual_row_index <= 25:
+            allowed_height_diffs.append(1)
+            allowed_height_diffs.append(2)
+            allowed_height_diffs.append(3)
+        for height_diff in allowed_height_diffs:
+            actual_height = actual_row_index + height_diff
+            if actual_height < 0 or actual_height >= img.shape[1]:
+                block_brightnesses[height_diff] = -1
+                continue
+            block_brightnesses[height_diff] = 255 if img[column_index, actual_height][0] == 255 else 0
+        brightest = max(block_brightnesses, key=block_brightnesses.get)
+        if brightest == 0 and block_brightnesses[0] == block_brightnesses[1] and len(row_steps) > 100 and sum([0 if x[0] == 0 else 1 for x in row_steps][-100:]) == 0:
+            tendency = 1 if sum([x[0] for x in row_steps]) > 0 else -1
+            row_steps.append((tendency, block_brightnesses[tendency]))
+            actual_row_index += tendency
+        else:
+            row_steps.append((brightest, block_brightnesses[brightest]))
+            actual_row_index += brightest
+    avg_row_brightness = sum(map(lambda row:row[1], row_steps)) / (len(row_steps))
+    if int(avg_row_brightness) >= 254:
+        print("storing row for later!")
+        return(row_index, [instruction[0] for instruction in row_steps])
+    return None
+
+
 
 def contrast(img):
     lab= cv.cvtColor(img, cv.COLOR_BGR2LAB)
